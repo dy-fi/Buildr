87e08f5d12aae7d18a210e2489c96c5d998a8fe6
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-13 01:10:16 -0800

    CRUD projects routes done - needs revision

--- controllers/projects.js
+++ controllers/projects.js
@@ -9,7 +9,7 @@ module.exports = (app) => {
     app.get('/projects', (req, res) => {
         const currentUser = req.user;
         Project.find()
-            .then(post => {
+            .then(project => {
                 console.log(`currentUser: ${currentUser}`)
                 res.render('projects-index', { project: project, currentUser: currentUser });
             })
@@ -22,5 +22,66 @@ module.exports = (app) => {
     app.get('/projects/new', (req, res) => {
         res.render('new-project', {});
     })
+
+    // CREATE a project
+    app.post('/projects/new', (req, res) => {
+        if (req.user) {
+            const project = new Project(req.body);
+            project.author = req.user._id;
+            project.save().then(project => {
+                return User.findById(req.user._id);
+            }).then(user => {
+                user.projects.unshift(project);
+                user.save();
+                res.redirect('/projects');
+            })
+                .catch(err => {
+                    console.log(err.message);
+                });
+        } else {
+            return res.status(401); // UNAUTHORIZED
+        }
+    });
+
+    // SHOW one project
+    app.get('/projects/:id', function (req, res) {
+        const currentUser = req.user;
+        // LOOK UP THE PROJECT
+        Project.findById(req.params.id).populate('author')
+            .then(post => {
+                res.render('show-project', { post, currentUser });
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    }); 
+
+    // UPDATE a project
+    // TODO: install method override
+    // app.get >>> /project/projectId/edit >> edit-project.hbs
+    app.get('/projects/:id/edit', (req, res) => {
+        Project.findById(req.params.id, function (err, project) {
+            res.render('edit-project', { project: project });
+        })
+    });
+
+    // app.put >>> /project/projectId >>> redirect show-project.hbs
+    app.put('/projects/:id', (req, res) => {
+        Project.findByIdAndUpdate(req.params.id, req.body)
+            .then(project => {
+                res.redirect(`/projects/${project._id}`)
+            })
+            .catch(err => {
+                console.log(err.message)
+            })
+    })
+    // DELETE
+    app.delete('/projects/:id/edit', function (req, res) {
+        Project.findByIdAndRemove(req.params.id).then((project) => {
+            res.redirect('/projects');
+        }).catch((err) => {
+            console.log(err.message);
+        })
+    }) 
 
 }
