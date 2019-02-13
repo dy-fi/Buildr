16ac6a14f9fa02b996bf5a6e6ecf40cd9ca91b3f
j-n4m4573 <jg_220@yahoo.com>
2019-02-12 22:47:33 -0800

    updates loginVC

--- Buildr/Buildr/LoginVC.swift
+++ Buildr/Buildr/LoginVC.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginVC: UIViewController {
 
     @IBOutlet weak var userNameField: UITextField!
     
     @IBOutlet weak var userPassField: UITextField!
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
