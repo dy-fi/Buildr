18ad955e9397f7e3498b725336bcf5592bb527ca
j-n4m4573 <jg_220@yahoo.com>
2019-02-12 22:43:54 -0800

    adds loginVC

---
+++ Buildr/Buildr/User.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Buildr
+//
+//  Created by Jamar Gibbs on 2/12/19.
+//  Copyright © 2019 B3773R. All rights reserved.
+//
+
+import Foundation
+
+struct User  {
+    
+    var userName : String
+    var userPassword : String
+    
+    init(userName: String, userPassword: String) {
+        self.userName = userName
+        self.userPassword = userPassword
+    }
+    
+}
---
+++ Buildr/Buildr/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Buildr
+//
+//  Created by Jamar Gibbs on 2/12/19.
+//  Copyright © 2019 B3773R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userNameField: UITextField!
+    
+    @IBOutlet weak var userPassField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func userAuth(_ sender: UIButton) {
+    }
+    
+}
+
